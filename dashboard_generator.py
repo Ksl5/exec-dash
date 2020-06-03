e1d9f4643231c6687b647943b7715c1abb892111
Ksl5 <65416583+Kiranlutfeali@users.noreply.github.com>
2020-06-03 02:28:18 -0400

    total monthly sales

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -1,13 +1,38 @@
 # dashboard_generator.py
+import csv
+import os
+
+def to_usd(my_price):
+   return f"${my_price:,.2f}" #> $12,000.71
+
+csv_filepath = "data/monthly-sales/sales-201803.csv"
+total_sale = 0
+
+with open(csv_filepath, "r") as csv_file: # "r" means "open the file for reading"
+    reader = csv.DictReader(csv_file) # assuming your CSV has headers
+    # reader = csv.reader(csv_file) # if your CSV doesn't have headers
+    for row in reader:
+        #print(row["sales price"])
+        total_sale = total_sale + float(row["sales price"])
+        
+
+
 
 print("-----------------------")
+month = "MARCH" # TODO: get from file name or date values
+year = 2018 # TODO: get from file name or date values
+print(f"SALES REPORT!")
+print(f"MONTH: {month} {year}")
+
 print("MONTH: March 2018")
 
 print("-----------------------")
 print("CRUNCHING THE DATA...")
 
 print("-----------------------")
-print("TOTAL MONTHLY SALES: $12,000.71")
+
+print("Total Monthly Sales: " + to_usd(total_sale))
+#print("TOTAL MONTHLY SALES: $12,000.71")
 
 print("-----------------------")
 print("TOP SELLING PRODUCTS:")
