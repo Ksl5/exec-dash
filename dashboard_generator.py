07316b1dc102f5d2c01534f38945105da7246007
Ksl5 <65416583+Kiranlutfeali@users.noreply.github.com>
2020-06-04 00:26:12 -0400

    open desired csv

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -5,7 +5,10 @@ import os
 def to_usd(my_price):
    return f"${my_price:,.2f}" #> $12,000.71
 
-csv_filepath = "data/monthly-sales/sales-201803.csv"
+CSV_FILENAME = "sales-201803.csv"
+
+csv_filepath = os.path.join("data/monthly-sales", CSV_FILENAME)
+#csv_filepath = "data/monthly-sales/sales-201803.csv"
 total_sale = 0
 
 with open(csv_filepath, "r") as csv_file: # "r" means "open the file for reading"
