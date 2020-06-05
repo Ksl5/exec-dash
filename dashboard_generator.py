1448808bb6bad8330fcb5961991d832fa7d9b9dc
Ksl5 <65416583+Kiranlutfeali@users.noreply.github.com>
2020-06-05 03:15:07 -0400

    top selling products

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -4,6 +4,7 @@ import os
 from datetime import datetime
 import matplotlib.pyplot as plt
 from operator import itemgetter
+import itertools
 
 
 
@@ -11,21 +12,42 @@ def to_usd(my_price):
    return f"${my_price:,.2f}" #> $12,000.71
 
 #selected_report = csv_filename
-CSV_FILENAME = input("Please enter date of monthly sales report (sales-YYYYMM.csv): ")
+#CSV_FILENAME = input("Please enter date of monthly sales report (sales-YYYYMM.csv): ")
 
-#CSV_FILENAME = "sales-201803.csv"
+CSV_FILENAME = "sales-201803.csv"
 
 csv_filepath = os.path.join("data/monthly-sales", CSV_FILENAME)
 #csv_filepath = "data/monthly-sales/sales-201803.csv"
 total_sale = 0
+rows = []
 
 with open(csv_filepath, "r") as csv_file: # "r" means "open the file for reading"
     reader = csv.DictReader(csv_file) # assuming your CSV has headers
     for row in reader:
         
         total_sale = total_sale + float(row["sales price"])
-        #print(row["product"][2], row["sales price"][2])
-        #print(row["date"])
+    
+#with open(csv_filepath, "r") as csv_file: # "r" means "open the file for reading"
+    #reader = csv.DictReader(csv_file)
+    #for od in reader:
+        rows.append(dict(row)) # ideally we would transform all the prices from strings to floats here
+
+#sales_prices = [float(row["sales price"]) for row in rows] # list comprehension for mapping purposes!
+#total_sales = sum(sales_prices)
+
+product_sales = []
+
+sorted_rows = sorted(rows, key=itemgetter("product"))
+rows_by_product = itertools.groupby(sorted_rows, key=itemgetter("product")) 
+
+for product, product_rows in rows_by_product:
+    monthly_sales = sum([float(row["sales price"]) for row in product_rows]) # list comprehension for mapping purposes!
+    product_sales.append({"name": product, "monthly_sales": monthly_sales})
+
+sorted_product_sales = sorted(product_sales, key=itemgetter("monthly_sales"), reverse=True)
+top_sellers = sorted_product_sales[0:3] # first three items in a list :-)
+ 
+        
         
         #top_product = row["product"]
         #unique_products = set(top_product)
@@ -33,13 +55,10 @@ with open(csv_filepath, "r") as csv_file: # "r" means "open the file for reading
         #print(top_product)    
         #bar_data = [
 #plt.style.use('ggplot')
-#print(row["date"])
-#2018-03-06
+
 parsed_date = datetime.strptime(row["date"], "%Y-%m-%d")
 
 
-#print(str(report_month).strftime("%Y-%m"))
-#print(date)
 
 #x = [dat["product"]for dat in bar_data]
 #Viewers = [dat["sales price"]for dat in bar_data]
@@ -56,11 +75,8 @@ month = parsed_date.strftime("%B")
 year = parsed_date.year
 print(f"SALES REPORT!")
 print(f"MONTH: {month} {year}")
-
-
 print("-----------------------")
 print("CRUNCHING THE DATA...")
-
 print("-----------------------")
 
 print("Total Monthly Sales: " + to_usd(total_sale))
@@ -68,10 +84,19 @@ print("Total Monthly Sales: " + to_usd(total_sale))
 
 print("-----------------------")
 print("TOP SELLING PRODUCTS:")
+counter = 0
+for top_seller in top_sellers:
+    counter = counter + 1
+    product_name = top_seller["name"]
+    sales_usd = to_usd(top_seller["monthly_sales"])
+    print(f"  {counter}. {product_name} {sales_usd}")
 
-print("  1) Button-Down Shirt: $6,960.35")
-print("  2) Super Soft Hoodie: $1,875.00")
-print("  3) etc.")
+#print(top_sellers)   
+
+
+#print("  1) Button-Down Shirt: $6,960.35")
+#print("  2) Super Soft Hoodie: $1,875.00")
+#print("  3) etc.")
 
 print("-----------------------")
 print("VISUALIZING THE DATA...")
